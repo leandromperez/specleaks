12d3427cc7564fd428a98ed2c6d0de7aacf07a46
Leandro Perez <leandro.perez@celerative.com>
2018-08-02 10:03:55 -0300

    0.1.1 Hotfix: Added missing files
    Updated Podfile.lock

--- SpecLeaks/Classes/LeakTest.swift
+++ SpecLeaks/Classes/LeakTest.swift
@@ -1,5 +1,5 @@
 //
-//  Matchers.swift
+//  LeakTest.swift
 //  Nimble
 //
 //  Created by Leandro Perez on 01/04/2018.
@@ -8,67 +8,59 @@
 import Quick
 import Nimble
 
-public typealias LeakTestConstructor = () -> AnyObject
+//public typealias LeakTestConstructor = () -> AnyObject
 
-public struct LeakTest{
-    let constructor : LeakTestConstructor
-    
+public struct LeakTest {
+    let constructor: LeakTestConstructor
+
     public init(constructor:@escaping LeakTestConstructor) {
         self.constructor = constructor
     }
-    
+
     internal func isLeaking() -> Bool {
-        weak var leaked : AnyObject? = nil
-        
-        autoreleasepool{
-            
-            var evaluated : AnyObject? = self.constructor()
-            
-            if let vc = evaluated as? UIViewController{
-                _ = vc.view //To call viewDidLoad on the vc
-            }
-        
+        weak var leaked: AnyObject? = nil
+
+        autoreleasepool {
+
+            var evaluated: AnyObject? = self.constructor()
+
+            //To call viewDidLoad on the vc
+            view(evaluated)
+
             leaked = evaluated
             evaluated = nil
         }
-        
+
         return leaked != nil
     }
-    
-    internal func isLeaking<P>( when action : (P) -> Any) -> PredicateStatus where P:AnyObject {
-        weak var leaked : AnyObject? = nil
-        
+
+    internal func isLeaking<P>( when action: (P) -> Any) -> PredicateStatus where P: AnyObject {
+        weak var leaked: AnyObject? = nil
+
         var failed = false
-        var actionResult : Any? = nil
-        
+        var actionResult: Any? = nil
+
         autoreleasepool {
-            
-            var evaluated : P? = self.constructor() as? P
-            
+
+            var evaluated: P? = self.constructor() as? P
+
             if evaluated == nil {
                 failed = true
-            }
-            else{
+            } else {
                 actionResult = action(evaluated!)
-                
-                if let vc = evaluated as? UIViewController{
-                    _ = vc.view //To call viewDidLoad on the vc
-                    vc.view = nil
-                }
-                
+
+                //To call viewDidLoad on the vc
+                view(evaluated)
+
                 leaked = evaluated
                 evaluated = nil
             }
         }
-        
-        if failed || actionResult == nil{
+
+        if failed || actionResult == nil {
             return PredicateStatus.fail
         }
-        
-        return PredicateStatus.init(bool:  leaked != nil)
+
+        return PredicateStatus.init(bool: leaked != nil)
     }
 }
-
-
-
-
