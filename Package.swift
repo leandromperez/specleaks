eff9c85401b5fe380ed0faca216fce18b672e641
mstankiewiczZen <69140528+mstankiewiczZen@users.noreply.github.com>
2020-11-24 12:09:16 +0100

    Bumped Quick and Nimble version for SPM (#22)

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
         .library(name: "SpecLeaks", targets: ["SpecLeaks"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Nimble", from: "8.0.7"),
-        .package(url: "https://github.com/Quick/Quick", from: "2.2.0"),
+        .package(url: "https://github.com/Quick/Nimble", from: "9.0.0"),
+        .package(url: "https://github.com/Quick/Quick", from: "3.0.0"),
     ],
     targets: [     
         .target(name: "SpecLeaks", dependencies: ["Quick", "Nimble"], path: "SpecLeaks/Classes/"),
